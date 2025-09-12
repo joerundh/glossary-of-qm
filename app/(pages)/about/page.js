89ade8e9f9132aa70151ec165e8e0fef85f5b504
joerundh <joerundh.p01@gmail.com>
2025-09-12 14:27:11 +0200

    added some lines, changed some typos

--- app/(pages)/about/page.js
+++ app/(pages)/about/page.js
@@ -2,11 +2,13 @@ export default function Page() {
     return (
         <>
             <h3 className={"w-full text-lg font-bold"}>About</h3>
-            <p className={"w-full"}>First off, this glossary is not extensive, in any sense whatever. It is a mix of claims about a set of concepts used in theoretical physics, even still after these hundred years. And being self-pressed for time limits the scope to some extend. Thus this glossary bares all the marks of a minimum viable product.</p>
-            <p className={"w-full"}>In that sense, then, it works as it is supposed to. The only thing it could be said to lack is material, of which (it might as well be said officially) there still is plenty unused.</p>
-            <p className={"w-full"}>Nevertheless, with the possible exception of some terminologies, everything here is at very leasy, with the proper effort, verifiably true.</p>
-            <p className={"w-full"}>And I dare say that the subject matter is almost maddeningly interesting, and only more so when one has penetrated the mathematics of it. Because it is much more mathematics-dependent branch of physics than the classical, in that what can be expressed using mathematics, cannot always so easily be expressed in mere words.</p>
-            <p className={"w-full"}>And everything is made more fun simply by being somewhat capable at it.</p>
+            <p className={"w-full"}>First off, this glossary is not extensive, in any sense whatever. It is a mix of claims about a set of concepts used in theoretical physics, even still after these hundred years. And the developer being self-pressed for time limits the scope to some extend. Thus this glossary bares all the marks of a minimum viable product.</p>
+            <p className={"w-full"}>In that sense, though, it works as it is supposed to. The only thing it could be said to lack is material, of which (it might as well be said officially) there still is plenty unused.</p>
+            <p className={"w-full"}>Nevertheless, with the possible exception of some terminologies, everything here is at the very least, with the proper effort laid down, verifiably true.</p>
+            <p className={"w-full"}>And some would claim (and they cannot be blamed for it) that the subject matter is near maddeningly interesting, and only more so when one has penetrated the mathematics of it. Because it is much more mathematics-dependent branch of physics than the classical one, in that what can be expressed using mathematics, cannot always so easily be expressed using mere words in a helpfully descriptive way. But that is the way it is.</p>
+            <p className={"w-full"}>And after all, everything is made more fun simply by being somewhat capable at it.</p>
+            <p className={"w-full"}>Very possibly, some more interlinking could be done in the entries (i.e. terms that appear in entries, which have their own entries, could be set to point to that entry). Given the limited amount of information in total, however, it is not considered a great hindrance to navigate between the entries, to some degree, via the entry list and/or the categories.</p>
+            <p className={"w-full"}>The developer bears the responibility of the correctness of the information given here. If any errors are found, even typos, they would be his fault.</p>
         </>
     )
 }
